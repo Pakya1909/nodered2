15cf2c40867e5ab75328fbac6e3b1c6fcf5f3436
jarihant3009 <arihantjain.min18@itbhu.ac.in>
2022-06-14 09:28:50 +0530

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nodered';
+
+  input = [
+    'inject' ,'catch','mqtt','http','websocket','tcp','udp','serial'
+  ]
+
+  output = [
+    'inject' ,'catch','mqtt'
+  ]
+
+  function = [
+    'function','exec'
+  ]
+ 
+  social = [
+    'catch','mqtt','http','websocket','tcp','udp'
+  ]
+
+  storage = [
+    'websocket','tcp','udp','serial'
+  ]
+
+  analysis = [
+    'inject' ,'catch','udp','serial'
+  ]
+
+  advanced = [
+    'http','websocket','tcp','udp','serial'
+  ]
+
+  nameofthefillernode : string = ''
+
+  nameoftheitem(item:string)
+  {
+    this.nameofthefillernode = item
+  }
+
+  tabs = ['flow-1'];
+  selected = new FormControl(0);
+
+  addTab(selectAfterAdding: boolean) {
+    this.tabs.push('Flow-'+ this.tabs.length);
+
+    if (selectAfterAdding) {
+      this.selected.setValue(this.tabs.length - 1);
+    }
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+  }
 }
